1c27b7a2fea2c8c56f42ac24a8b53bfbc32d782e
Raphael Arce <raphael.arce@ts.berlin>
2024-01-01 15:25:01 +0100

    removed unecessary newlines
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
