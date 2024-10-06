cdfb5e46855d00bfbc33516f99f7276a9466205a
Raphael Arce <raphael.arce@ts.berlin>
2024-10-06 21:46:12 +0200

    feat: update about page
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ src/content/about.ts
@@ -0,0 +1,121 @@
+export const experiences = [
+  {
+    title: "Senior Full-Stack Engineer",
+    company: "Technologiestiftung Berlin",
+    date: "Apr 2023 - Present",
+    link: "https://www.technologiestiftung-berlin.de/",
+    subtitle:
+      "Open source prototypes/products I have worked on during my time at Technologiestiftung Berlin: ",
+    products: [
+      {
+        previewLink: "https://anmelde-check.vercel.app/",
+        linkLabel: "Anmelde-Check",
+        description:
+          ", a wizard that assists people in figuring out which documents they need to register at a Bürgeramt in Berlin.",
+        githubLink: "https://github.com/technologiestiftung/anmelde-check",
+      },
+      {
+        previewLink: "https://www.parla.berlin/",
+        linkLabel: "Parla",
+        description:
+          ", an AI search engine for written inquiries of the berliner House of Deputies (AGH).",
+        githubLink: "https://github.com/technologiestiftung/parla-frontend",
+      },
+      {
+        previewLink: "https://future-greetings.vercel.app/",
+        linkLabel: "Idea Machine",
+        description:
+          ", an AI physical computing prototype that generates digital product ideas for Berlin.",
+        githubLink:
+          "https://github.com/technologiestiftung/idea-machine-backend",
+      },
+      {
+        previewLink: "https://chat.oeffentliches-gestalten.de/",
+        linkLabel: "Chatbot Öffentliches Gestalten",
+        description:
+          ", a proof-of-concept AI chatbot to answer questions about the book <i>Public Service Design Handbook</i>.",
+        githubLink:
+          "https://github.com/technologiestiftung/oeffentliches-gestalten-gpt-search",
+      },
+      {
+        previewLink: "https://giessdenkiez.de/",
+        linkLabel: "Giess den Kiez",
+        description:
+          ", a web app that enables coordinated citizen participation in the irrigation of urban trees.",
+        githubLink: "https://github.com/technologiestiftung/giessdenkiez-de",
+      },
+    ],
+  },
+  {
+    title: "Full-Stack Engineer",
+    company: "Acker e. V.",
+    date: "Feb 2020 - Apr 2023",
+    link: "https://www.acker.co/",
+    subtitle: "Products I have worked on during my time at Acker e. V.: ",
+    products: [
+      {
+        previewLink: "https://www.acker.co/",
+        linkLabel: "acker.co",
+        description:
+          ", a new website as part of a brand relaunch with subpages for each program. ",
+        githubLink: "",
+      },
+      {
+        previewLink: "https://mein.acker.co/",
+        linkLabel: "mein.acker.co",
+        description:
+          ", an educational platform with personalized content for educators to enhance their curriculum with Acker's programs. ",
+        githubLink: "",
+      },
+      {
+        previewLink: "https://apps.apple.com/de/app/ackerpause/id1515122523",
+        linkLabel: "Ackerpause",
+        description:
+          ", an educational platform with personalized content for educators to enhance their curriculum with Acker's programs. ",
+        githubLink: "",
+      },
+    ],
+  },
+  {
+    title: "Working student",
+    company: "SNET - TU Berlin",
+    date: "Nov 2018 - Dez 2019",
+    link: "https://www.tu.berlin/snet",
+    subtitle:
+      "Development of decentralised, privacy-oriented applications for self-sovereign identities using blockchain technologies.",
+    products: [],
+  },
+  {
+    title: "Working student",
+    company: "Carmeq GmbH (now CARIAD)",
+    date: "Apr 2015 - Jun 2017",
+    link: "https://www.linkedin.com/company/carmeq-gmbh/",
+    subtitle:
+      "Implementation of voice control prototypes for commanding car infotainment systems using speech recognition, natural language understanding, and text-to-speech technology.",
+    products: [],
+  },
+];
+
+export const degrees = [
+  {
+    degree: "Master of Science",
+    program: "Computer Science",
+    institution: "TU Berlin",
+    date: "Dec 2021",
+    link: "https://www.tu.berlin/",
+  },
+  {
+    degree: "Bachelor of Science",
+    program: "Engineering Informatics",
+    institution: "HTW Berlin",
+    date: "Apr 2016",
+    link: "https://www.htw-berlin.de/",
+  },
+  {
+    degree: "Baccalauréat",
+    program: "série scientifique",
+    institution: "Lycée Gustave Flaubert (TN)",
+    date: "Jun 2012",
+    link: "https://www.erlm.tn/lgf/",
+  },
+];
